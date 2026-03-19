befecc3f0505b4b8cd1bc5712ace604fa62d3842
jaffar2022 <jaffarm048@gmail.com>
2026-03-19 21:14:04 +0530

    Assignment added

---
+++ Flask and MongoDB/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Replace with your MongoDB Atlas URI
+client = MongoClient("mongodb+srv://dummy:1234@cluster0.cxmny1h.mongodb.net/?appName=Cluster0")   
+db = client['mydb']
+collection = db['users']
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    try:
+        name = request.form['name']
+        email = request.form['email']
+
+        collection.insert_one({
+            "name": name,
+            "email": email
+        })
+
+        return redirect(url_for('success'))
+
+    except Exception as e:
+        return render_template('form.html', error=str(e))
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
